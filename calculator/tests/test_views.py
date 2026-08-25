175b2bf7018f35cae5532b08b9ca8ef1c8be31b6
Karolina <chodor.karolina@gmail.com>
2026-08-25 14:17:02 +0200

    test: add results page integration test

--- calculator/tests/test_views.py
+++ calculator/tests/test_views.py
@@ -10,6 +10,7 @@ User = get_user_model()
 
 class CalculatorViewTest(TestCase):
 
+    # STRONA KALKULATORA DZIAŁA
     def test_calculator_page_works(self):
         response = self.client.get(
             reverse("calculator")
@@ -87,4 +88,66 @@ class CalculatorViewTest(TestCase):
         self.assertEqual(
             Calculation.objects.count(),
             0,
+        )
+
+    # PEŁNY PRZEPŁYW - FORMULARZ → WYNIKI
+    def test_calculation_results_page_renders_after_post(self):
+        user = User.objects.create_user(
+            username="result_user",
+            password="test12345",
+        )
+
+        self.client.login(
+            username="result_user",
+            password="test12345",
+        )
+
+        response = self.client.post(
+            reverse("calculator"),
+            data={
+                "name": "Test wyniku",
+                "adults": 2,
+                "children": 1,
+                "pets": 1,
+                "remote_work": True,
+                "hobby": True,
+                "bikes": 2,
+                "storage_level": "medium",
+            },
+            follow=True,
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+        self.assertTemplateUsed(
+            response,
+            "calculator/results.html",
+        )
+
+        self.assertContains(
+            response,
+            "Wynik obliczenia",
+        )
+
+        self.assertContains(
+            response,
+            "Test wyniku",
+        )
+
+        self.assertContains(
+            response,
+            "Edytuj obliczenie",
+        )
+
+        self.assertContains(
+            response,
+            "Usuń obliczenie",
+        )
+
+        self.assertContains(
+            response,
+            "Moje obliczenia",
         )
